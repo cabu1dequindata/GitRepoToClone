518c6784bb4ff10c95eb023c0519326fc0cde189
cabu1dequindata <40891910+cabu1dequindata@users.noreply.github.com>
2018-07-06 15:46:45 +0200

    Initial Upload SCs

---
+++ Workspace.jdi/LocalDevelopment/RWZ-TEST/quindata.com/demo/portal/_comp/src.core/de/quindata/demo/MyPortalComponent.java
@@ -0,0 +1,11 @@
+package de.quindata.demo;
+ 
+import com.sapportals.portal.prt.component.*;
+
+public class MyPortalComponent extends AbstractPortalComponent
+{
+    public void doContent(IPortalComponentRequest request, IPortalComponentResponse response)
+    {
+
+    }
+}
